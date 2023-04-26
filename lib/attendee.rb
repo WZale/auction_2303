633a84c4b7fb58bad2880b69140bbe64791414ed
William Zale <williamzale@gmail.com>
2023-04-26 11:12:12 -0400

    Add initial class structure

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -1,3 +1,9 @@
 class Attendee
-  
+  attr_reader :name,
+              :budget
+
+  def initialize (attendee_data)
+    @name = attendee_data[:name]
+    @budget = attendee_data[:budget].delete("$").to_i
+  end
 end
