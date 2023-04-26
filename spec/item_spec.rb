6daa647b4c26d78e8177b102da7215a7ae16bcd9
William Zale <williamzale@gmail.com>
2023-04-26 11:06:27 -0400

    Initial commit

---
+++ spec/item_spec.rb
@@ -0,0 +1,12 @@
+require './lib/item'
+
+RSpec.describe Item do
+  before(:each) do
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+  end
+
+  it 'can initialize with readable attributes' do
+    expect(@item1.name).to eq('Chalkware Piggy Bank')
+  end
+end
