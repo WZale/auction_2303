fc930476000b4b55c308bd7b44412dfeab788b0e
William Zale <williamzale@gmail.com>
2023-04-26 12:02:04 -0400

    Add tests for add_bid and current_high_bid methods

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,12 +1,41 @@
 require './lib/item'
+require './lib/attendee'
 
 RSpec.describe Item do
   before(:each) do
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
+
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
   end
 
   it 'can initialize with readable attributes' do
     expect(@item1.name).to eq('Chalkware Piggy Bank')
+    expect(@item1.bids).to eq({})
   end
+
+  it 'has an add_bid method' do
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+
+    expected =  {
+                @attendee2 => 20,
+                @attendee1 => 22
+                }
+
+    expect(@item1.bids).to eq(expected)
+  end
+
+  it 'has a current_high_bid method' do
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+
+    expect(@item1.current_high_bid).to eq(22)
+  end
+
 end
