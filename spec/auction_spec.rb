40a33953e27040a79786aa91f892a1fc7a52b844
William Zale <williamzale@gmail.com>
2023-04-26 12:31:37 -0400

    Add test for potential_revenue method

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -48,6 +48,17 @@ RSpec.describe Auction do
   end
 
   it 'has a potential revenue method' do
-    
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    @auction.add_item(@item3)
+    @auction.add_item(@item4)
+    @auction.add_item(@item5)
+
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+    @item4.add_bid(@attendee3, 50)
+    @item3.add_bid(@attendee2, 15)
+
+    expect(@auction.potential_revenue).to eq(87)
   end
 end
