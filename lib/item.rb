fd06bac8c340f568b082e6af5beed5566e493719
William Zale <williamzale@gmail.com>
2023-04-26 12:01:16 -0400

    Add add_bid and current_high_bid methods

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,18 @@
 class Item
-  attr_reader :name
-  
+  attr_reader :name,
+              :bids
+
   def initialize(name)
     @name = name
+    @bids = {}
   end
+
+  def add_bid(attendee, amount)
+    @bids[attendee] = amount
+  end
+
+  def current_high_bid
+    self.bids.values.max
+  end
+
 end
